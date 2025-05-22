80ff20e990fd54260d42e02db51b719dea7ae494
Walisson Santos <walissonsantost008@gmail.com>
2025-05-22 13:43:58 -0300

    Initial commit

---
+++ src/main/java/com/cadastro/usuarios/UsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.cadastro.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosApplication.class, args);
+	}
+
+}
