2332c7fe581ddf4c09653e84e3587523ca342188
Walisson Santos <walissonsantost008@gmail.com>
2025-05-23 18:22:57 -0300

    Remoção dos metodos put, delete e post.

--- src/main/java/com/cadastro/usuarios/controller/TarefasController.java
+++ src/main/java/com/cadastro/usuarios/controller/TarefasController.java
@@ -5,9 +5,7 @@ import com.cadastro.usuarios.services.TarefasService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,24 +26,4 @@ public class TarefasController {
         Tarefas tarefas = service.findById(id);
         return ResponseEntity.ok().body(tarefas);
     }
-
-    @PostMapping
-    public ResponseEntity<Tarefas> insert(@RequestBody Tarefas tarefas){
-        tarefas = service.insert(tarefas);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(tarefas.getId()).toUri();
-        return ResponseEntity.created(uri).body(tarefas);
-    }
-
-    @PutMapping(value = "/{id}")
-    public ResponseEntity<Tarefas> update(@PathVariable Long id,@RequestBody Tarefas tarefas){
-        tarefas = service.update(id, tarefas);
-        return ResponseEntity.ok().body(tarefas);
-    }
-
-    @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
-        service.delete(id);
-        return ResponseEntity.noContent().build();
-    }
-
 }
